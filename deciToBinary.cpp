65ca7ece0ae2bc718c871c8fb3db946e85c37ecb
Abhijeetrana11 <91862109+Abhijeetrana11@users.noreply.github.com>
2022-04-22 09:25:45 +0530

    Add files via upload

---
+++ deciToBinary.cpp
@@ -0,0 +1,26 @@
+// n=10
+// fir n ko 2 se devide kro or reminder store kro
+// (fir stored answer ko 2 se devide kro)
+// third proces ko jbtk repeatkrte jao tb tk divisor answer zero na ho jaye
+
+// division          remainder
+// 10/2 >5            0
+// 5/2 >2             1
+// 2/2 >1             0
+// 1/2 >0             1
+// stop 
+//  0101 ko reverse krdo
+//  1010 is the binary form of 10
+
+// n=7
+// divisor               remainder
+// 7/2 > 3                  
+// 3/2 >1                   1
+// 1/2 > 0                  1
+// rever ans =111
+
+// let given integer are 1,2,3
+// to make a no. from these integer in same flow we use 
+// ans = (ans + 10)+ digit
+// and for reverse order-:
+// ans = (ans + pow(10,i))+ digit
